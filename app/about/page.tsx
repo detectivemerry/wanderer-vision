63a2833ccb6cd745bfd6c9974a8f7178cd3acb0c
detectivemerry <jiiasheng@hotmail.com>
2025-02-23 13:27:23 +0800

    add button to about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,17 @@
-"use client"
+"use client";
 import React from "react";
+import Link from "next/link";
+import { Button } from "@mui/material";
 
 export default async function about() {
   return (
     <div className="flex justify-center gap-8 mx-[20%] mt-16">
-      <div className = "container flex-1 flex justify-center">
-        <img src="about/clinton_dp.png"/>
+      <div className="container flex-1 flex justify-center">
+        <img src="about/clinton_dp.png" />
       </div>
       <div className="flex flex-col flex-1 mt-8">
         <div className="font-bold text-center mb-12">ABOUT CLINTON LEE</div>
-        <div className = "px-8">
+        <div className="px-8">
           Hey there! I'm Clinton, a freelance cameraman based in Singapore with
           a passion for capturing life’s best moments. I started my journey
           working under a professional photography company, where I gained
@@ -20,8 +22,19 @@ export default async function about() {
           corporate event, I aim to tell your story through natural, heartfelt,
           and high-quality images. Let’s create something amazing together! 📸
         </div>
-        <div>
-          <div></div>
+        <div className="flex justify-center m-28">
+          <Link href="/contact" className="no-underline">
+            <Button
+              variant="contained"
+              sx={{
+                color: "#333333",
+                fontWeight: "bold",
+                backgroundColor: "#FBEEAC",
+              }}
+            >
+              Get in touch
+            </Button>
+          </Link>
         </div>
       </div>
     </div>
